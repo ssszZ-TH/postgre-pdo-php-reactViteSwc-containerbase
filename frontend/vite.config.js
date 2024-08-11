7767fd4822e7383d9cff0a4ada67e79470c85d41
sitthipong_tufA15 <sitthipong.cham@gmail.com>
2024-08-11 19:01:47 +0700

    run only react its work

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',  // เพื่อให้เข้าถึงจากภายนอก
+    port: 4173,       // เปลี่ยนพอร์ตให้ตรงกับการ expose ใน Dockerfile
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: 4173,
+  }
 })
