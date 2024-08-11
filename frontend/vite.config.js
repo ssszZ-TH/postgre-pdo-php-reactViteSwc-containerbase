f2bc6b7ea0361451af1405fa6736c0567bfcf5ff
sitthipong_tufA15 <sitthipong.cham@gmail.com>
2024-08-11 22:27:20 +0700

    now react work with php

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,11 +5,11 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0',  // เพื่อให้เข้าถึงจากภายนอก
-    port: 4173,       // เปลี่ยนพอร์ตให้ตรงกับการ expose ใน Dockerfile
-  },
-  preview: {
-    host: '0.0.0.0',
-    port: 4173,
+    host: true,
+    port: 3000,
+    watch: {
+      usePolling: true,
+      interval: 500
+    }
   }
 })
